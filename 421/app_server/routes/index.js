9ff244cac9c10de878b060c1d82ce27b35519c7f
jldaggs <daggsjillian@gmail.com>
2024-03-23 15:22:43 -0400

    app app implimentation

---
+++ 421/app_server/routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var ctrlBlogs = require('../controllers/ctrlBlog.js')
+
+/* GET home page. */
+router.get('/', ctrlBlogs.home);
+router.get('/blogs', ctrlBlogs.blogList)
+router.get('/blogs/add', ctrlBlogs.blogAdd)
+router.get('/blogs/delete/:id', ctrlBlogs.blogDelete)
+router.get('/blogs/edit/:id', ctrlBlogs.blogEdit)
+
+module.exports = router;
