60c3014623ba4156806e6746791f55823f0c59ca
jldaggs <daggsjillian@gmail.com>
2024-03-23 19:09:50 -0400

    fixes

--- 421/app_api/routes/index.js
+++ 421/app_api/routes/index.js
@@ -7,7 +7,7 @@ router.get('/blogs', ctrlBlogs.getAllBlogs);
 router.get('/blogs/:id', ctrlBlogs.getBlogById);
 router.post('/blogs/create', ctrlBlogs.createBlog);
 router.get('/blogs/edit/:id', ctrlBlogs.editBlogForm);
-router.post('/blogs/edit/:id', ctrlBlogs.editBlogSubmit);
+router.post('/blogs/edit/:id', ctrlBlogs.editBlogConfirm);
 router.get('/blogs/delete/:id', ctrlBlogs.deleteBlogConfirm);
 router.post('/blogs/delete/:id', ctrlBlogs.deleteBlog);
 
