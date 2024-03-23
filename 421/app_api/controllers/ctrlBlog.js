bcd988d975be188ab346b37bf328f1e8cb78fbfc
jldaggs <daggsjillian@gmail.com>
2024-03-23 15:28:00 -0400

    fixes

---
+++ 421/app_api/controllers/ctrlBlog.js
@@ -0,0 +1,61 @@
+const Blog = require('../models/blog');
+
+// Get all blogs
+exports.getAllBlogs = async (req, res) => {
+    try {
+        const blogs = await Blog.find();
+        res.json(blogs);
+    } catch (error) {
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
+
+// Get a single blog by ID
+exports.getBlogById = async (req, res) => {
+    try {
+        const blog = await Blog.findById(req.params.id);
+        if (!blog) {
+            return res.status(404).json({ error: 'Blog not found' });
+        }
+        res.json(blog);
+    } catch (error) {
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
+
+// Add a new blog
+exports.addBlog = async (req, res) => {
+    try {
+        const newBlog = new Blog(req.body);
+        const savedBlog = await newBlog.save();
+        res.status(201).json(savedBlog);
+    } catch (error) {
+        res.status(400).json({ error: 'Failed to add blog' });
+    }
+};
+
+// Update a blog by ID
+exports.updateBlog = async (req, res) => {
+    try {
+        const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!updatedBlog) {
+            return res.status(404).json({ error: 'Blog not found' });
+        }
+        res.json(updatedBlog);
+    } catch (error) {
+        res.status(400).json({ error: 'Failed to update blog' });
+    }
+};
+
+// Delete a blog by ID
+exports.deleteBlog = async (req, res) => {
+    try {
+        const deletedBlog = await Blog.findByIdAndDelete(req.params.id);
+        if (!deletedBlog) {
+            return res.status(404).json({ error: 'Blog not found' });
+        }
+        res.sendStatus(204);
+    } catch (error) {
+        res.status(400).json({ error: 'Failed to delete blog' });
+    }
+};
