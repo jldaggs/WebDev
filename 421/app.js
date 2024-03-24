c72a15ff85fb910cdb4f160be16e2dd75423be9d
jldaggs <daggsjillian@gmail.com>
2024-03-24 15:35:25 -0400

    fixes

--- 421/app.js
+++ 421/app.js
@@ -11,17 +11,26 @@ var routesApi = require('./app_api/routes/index');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname,'app_server', 'views'));
-app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname,'app_server', 'views'));
+// app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'app_client')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+//app.use('/', routes);
 app.use('/api', routesApi);
+
+app.use((req, res, next) => {
+  if (!req.path.startsWith('/api')) {
+    res.sendFile(path.join(__dirname, 'app_client', 'index.html'));
+  } else {
+    next(createError(404)); // If it's an unmatched API route, return 404
+  }
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
