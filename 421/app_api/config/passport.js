f7b36fb359e292c0e7069cbd88b72bd84dab04da
jldaggs <daggsjillian@gmail.com>
2024-04-01 22:30:25 -0400

    bugfixes

--- 421/app_api/config/passport.js
+++ 421/app_api/config/passport.js
@@ -2,7 +2,7 @@
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var mongoose = require('mongoose');
-var User = mongoose.model('User');
+const User = mongoose.model('User');
 
 passport.use(new LocalStrategy({
     usernameField: 'email'
--- 421/app_api/controllers/authentication.js
+++ 421/app_api/controllers/authentication.js
@@ -1,6 +1,6 @@
 var passport = require('passport');
 var mongoose = require('mongoose');
-var User = mongoose.model('User');
+const User = mongoose.model('User');
 
 var sendJSONresponse = function(res, status, content) {
   res.status(status);
