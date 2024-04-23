2e6d415123f77d6b8f2ae28835365d12d0376816
jldaggs <daggsjillian@gmail.com>
2024-04-23 06:46:12 -0400

    like implimentation?

--- 421/app_client/blogApp.js
+++ 421/app_client/blogApp.js
@@ -120,16 +120,16 @@ app.controller('blogListController', ['$scope', '$http', '$rootScope', 'AuthServ
             return;
         }
     
-        $http.post('/api/blog/' + blog._id + '/like').then(function(response) {
-            if (response.data.success) {
-                blog.likeCount = response.data.likeCount;
-                blog.isLikedByUser = response.data.liked;
-            }
+        $http.post('/api/blog/' + blog._id + '/toggle-like', {}, {
+            headers: { 'Authorization': 'Bearer ' + AuthService.getToken() }
+        }).then(function(response) {
+            blog.likeCount = response.data.likeCount;
+            blog.isLikedByUser = response.data.liked;
         }).catch(function(error) {
             console.error('Error toggling like:', error);
             alert('Failed to toggle like. Please try again.');
         });
-    };
+    };    
     $rootScope.$on('authChange', function() {
         loadBlogs();  // This will reload the blogs whenever the authentication state changes
     });
